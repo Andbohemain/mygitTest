a8f1d924faf5037089f6f5769c08716bdd41eb64
Andbohemain <Lf19990628@163.com>
2021-01-23 20:52:13 +0800

    分支

--- gittesdt01/src/main/java/A.java
+++ gittesdt01/src/main/java/A.java
@@ -6,5 +6,7 @@ public class A {
         System.out.println("这次跟新是分支的跟新");
         int a =90/2;
         System.out.println(a);
+        System.out.println(787878);
+        System.out.println("1111111111");
     }
 }
