c54a57e4d907752a499f28614eb87017db003055
Andbohemain <Lf19990628@163.com>
2021-01-23 20:55:12 +0800

    bancher01

--- gittesdt01/src/main/java/A.java
+++ gittesdt01/src/main/java/A.java
@@ -1,12 +1,5 @@
 public class A {
     public static void main(String[] args) {
-        System.out.println("isduifh");
-        System.out.println("sdf");
-        System.out.println("分支");
-        System.out.println("这次跟新是分支的跟新");
-        int a =90/2;
-        System.out.println(a);
-        System.out.println(787878);
-        System.out.println("1111111111");
+        //bancher01
     }
 }
