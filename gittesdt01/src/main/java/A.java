99513e7d1a6c353568711b5942e5e0017ce02a6a
Andbohemain <Lf19990628@163.com>
2021-01-23 20:45:40 +0800

    啊哈哈哈哈哈哈哈分支1222

--- gittesdt01/src/main/java/A.java
+++ gittesdt01/src/main/java/A.java
@@ -4,5 +4,7 @@ public class A {
         System.out.println("sdf");
         System.out.println("分支");
         System.out.println("这次跟新是分支的跟新");
+        int a =90/2;
+        System.out.println(a);
     }
 }
